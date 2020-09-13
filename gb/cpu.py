594f1f1f2ca895fe35d272a572fc99e44f7a7ed6
Adam Van Prooyen <adamvp@forallsecure.com>
2020-09-13 12:33:45 -0700

    Rewrite cpu to support varying register widths/addressing modes

--- gb/cpu.py
+++ gb/cpu.py
@@ -1,43 +1,54 @@
-import json
-from typing import List, NamedTuple
+from collections import defaultdict
+from typing import Dict, List, NamedTuple
 
-OPCODES_PATH = "gb/opcodes.json"
-OPCODES = json.loads(open(OPCODES_PATH, "r").read())
+MASK_8BIT = 2 ** 8 - 1
+
+RawRegs = Dict[str, int]
 
 
-class Insn(NamedTuple):
-    mnem: str
-    ops: List[str]
-    cycles: int
-    size: int
-
+class Reg(NamedTuple):
+    name: str
+    parts: List[str]
+    def size(self):
+        return len(self.name) * 8  # fun hack
+    def mask(self):
+        return 2 ** self.size() - 1
+    def read(self, rawregs: RawRegs) -> int:
+        return sum(rawregs[r] << i * 8 for i, r in enumerate(reversed(self.parts)))
+    def write(self, rawregs: RawRegs, val: int):
+        for i, r in enumerate(reversed(self.parts)):
+            rawregs[r] = (val >> i * 8) & MASK_8BIT
     def __str__(self):
-        op_str = ", ".join(self.ops)
-        return "{} {}".format(self.mnem, op_str)
+        return self.name
+    def __repr__(self):
+        return "{}.{}".format(type(self).__name__, self.name)
 
 
-def n(b: bytes):
-    pass
+# 8 bit regs
+Reg.A = Reg("A", ["A"])
+Reg.F = Reg("F", ["F"])  # not individually addressable
+Reg.B = Reg("B", ["B"])
+Reg.C = Reg("C", ["C"])
+Reg.D = Reg("D", ["D"])
+Reg.E = Reg("E", ["E"])
+Reg.H = Reg("H", ["H"])
+Reg.L = Reg("L", ["L"])
+
+# 16 bit compound regs
+Reg.AF = Reg("AF", ["A", "F"])
+Reg.BC = Reg("BC", ["B", "C"])
+Reg.DE = Reg("DE", ["D", "E"])
+Reg.HL = Reg("HL", ["H", "L"])
+
+# 16 bit regs
+Reg.PC = Reg("PC", ["PC"])
+Reg.SP = Reg("SP", ["SP"])
 
 
-def nn(bb: bytes):
-    pass
-
-
-def disass(bs: bytes) -> Insn:
-    if bs[0] == 0xCB:  # opcode prefix
-        opcodes = OPCODES["cbprefixed"]
-        opcode = bs[1]
-    else:
-        opcodes = OPCODES["unprefixed"]
-        opcode = bs[0]
-
-    insn = opcodes[hex(opcode)]
-
-    mnem = insn["mnemonic"]
-    ops = [insn.get("operand1"), insn.get("operand2")]
-    ops = [op for op in ops if op is not None]
-    cycles = insn["cycles"][0]
-    size = insn["length"]
-
-    return Insn(mnem, ops, cycles, size)
+class CPU:
+    def __init__(self):
+        self._rawregs: RawRegs = defaultdict(int)
+    def load(self, reg: Reg) -> int:
+        return reg.read(self._rawregs)
+    def store(self, reg: Reg, val: int):
+        reg.write(self._rawregs, val)
