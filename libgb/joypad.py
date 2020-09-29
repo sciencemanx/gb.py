2800a92f6b17fd289a318ed4f82988bc0be1c9d8
Adam Van Prooyen <adamvp@forallsecure.com>
2020-09-28 20:58:46 -0700

    Add joypad support

--- libgb/joypad.py
+++ libgb/joypad.py
@@ -1,9 +1,42 @@
+import pygame
+
 from .io import IOHandler, JoypadIO
 
+JOYP_DIR_FLAG = 1 << 4
+JOYP_BUTTON_FLAG = 1 << 5
+JOYP_MODE_MASK = JOYP_DIR_FLAG | JOYP_BUTTON_FLAG
+
+DIR_KEYS = {
+    pygame.K_RIGHT: 1 << 0,
+    pygame.K_LEFT: 1 << 1,
+    pygame.K_UP: 1 << 2,
+    pygame.K_DOWN: 1 << 3,
+}
+
+BUTTON_KEYS = {
+    pygame.K_z: 1 << 0,
+    pygame.K_x: 1 << 1,
+    pygame.K_RSHIFT: 1 << 2,
+    pygame.K_RETURN: 1 << 3,
+}
+
 class JoypadIOHandler(IOHandler):
+    mode: int
+    def __init__(self):
+        self.mode = 0
     def __contains__(self, addr: int) -> bool:
         return addr == JoypadIO.JOYP.value
     def load(self, addr: int) -> int:
-        return 0
+        keys = pygame.key.get_pressed()
+        joyp = self.mode
+        if (joyp & JOYP_DIR_FLAG) == 0:
+            for dir, flag in DIR_KEYS.items():
+                if keys[dir]:
+                    joyp |= flag
+        if (joyp & JOYP_BUTTON_FLAG) == 0:
+            for button, flag in BUTTON_KEYS.items():
+                if keys[button]:
+                    joyp |= flag
+        return joyp
     def store(self, addr: int, val: int):
-        pass
+        self.mode = val & JOYP_MODE_MASK
