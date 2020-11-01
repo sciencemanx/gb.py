3add97fd6a0cfff75c56302988631608ab503718
Adam Van Prooyen <adamvp@forallsecure.com>
2020-11-01 12:25:49 -0500

    Change profiling to find hot loops

--- libgb/prof.py
+++ libgb/prof.py
@@ -1,44 +1,53 @@
 from collections import Counter, deque
-from typing import Deque
+import enum
+from typing import Any, Deque, Dict
 
-history: Deque[str]
-hits: Counter
-hot_spots: Counter
+history: Deque[Any]
+loop_count: Counter
+loops: Dict[int, Any]
 
 loop_start = 0
-loop_count: Counter = Counter()
+total = 0
 
-def init(n=3):
+def init(maxlen=100):
     global history
-    global hits
-    global hot_spots
     global loop_count
-    history = deque(maxlen=n)
-    hits = Counter()
-    hot_spots = Counter()
+    global loops
+    history = deque(maxlen=maxlen)
     loop_count = Counter()
+    loops = {}
 
 
 def update(pc, next_pc, insn):
+    global total
+    history.appendleft((pc, insn))
+    total += insn.cycles
+
     if insn.step == 0:
         global loop_start
         if loop_start == next_pc:
             loop_count[(loop_start, pc)] += 1
+            if loop_start not in loops:
+                for i, (addr, _) in enumerate(history):
+                    if addr == loop_start:
+                        break
+                else:
+                    i = None
+                if i is not None:
+                    loops[loop_start] = list(history)[:i+1][::-1]
+
         loop_start = next_pc
 
-    op = insn.mnem.split()[0]
-    history.append(op)
-    hits[tuple(history)] += 1
-    hot_spots[(pc,insn.mnem)] += 1
 
 
 def show(n=30):
-    print("\nngrams:")
-    for hit, n_hits in hits.most_common(n):
-        print("{}: {}".format(n_hits, " -> ".join(hit)))
-    print("hotspots:")
-    for (spot, mnem), n_hits in hot_spots.most_common(200):
-        print("{}: {:04X} - {}".format(n_hits, spot, mnem))
     print("top loops:")
-    for (start, end), n_hits in loop_count.most_common(20):
-        print("{}: {:04X} - {:04X}".format(n_hits, start, end))
+    for (start, end), n_hits in loop_count.most_common(10):
+        print("{}:".format(n_hits))
+        if start in loops:
+            cost = sum(insn.cycles for _, insn in loops[start])
+            print("\t{:.2f}%".format(cost * n_hits / total * 100))
+            for addr, insn in loops[start]:
+                print("\t{:04X} - {}".format(addr, insn.mnem))
+        else:
+            print("\tloop too long ({:04X}:{:04X})".format(start, end))
